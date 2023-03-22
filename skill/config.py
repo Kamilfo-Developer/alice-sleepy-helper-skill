e8e56030878e52ba445bd176e71de8a435892de5
Kamil Biktimirov <Biktimirov.kamil@yandex.ru>
2023-03-22 06:14:49 +0300

    Added default values for settings

--- skill/config.py
+++ skill/config.py
@@ -1,9 +1,10 @@
 from dotenv import load_dotenv
+import os
 
 load_dotenv()
 
-WEBHOOK_URL_PATH = os.getenv("WEBHOOK_URL_PATH")
+WEBHOOK_URL_PATH = os.getenv("WEBHOOK_URL_PATH") or "/"
 
-WEBAPP_HOST = os.getenv("WEBAPP_HOST")
+WEBAPP_HOST = os.getenv("WEBAPP_HOST") or "localhost"
 
-WEBAPP_PORT = os.getenv("WEBAPP_PORT")
+WEBAPP_PORT = os.getenv("WEBAPP_PORT") or 5555
