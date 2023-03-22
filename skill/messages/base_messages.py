b7cdd0e150688db87d4f2db444d4d13d7f4bb8c7
spdz <spidepog@gmail.com>
2023-03-22 16:56:19 +0300

    Finished BaseMessages ABC

--- skill/messages/base_messages.py
+++ skill/messages/base_messages.py
@@ -1,5 +1,7 @@
 import abc
-from .context_flags import Daytime
+import datetime
+from ..utils import TextWithTTS
+from typing import Any, List
 
 
 class BaseMessages(abc.ABC):
@@ -7,11 +9,46 @@ class BaseMessages(abc.ABC):
         pass
 
     @abc.abstractmethod
-    def get_start_message_intro(self, time: Daytime):
+    def get_start_message_intro(self, time: datetime.datetime) -> TextWithTTS:
         pass
 
     @abc.abstractmethod
-    def get_start_message_comeback(self, time: Daytime):
+    def get_start_message_comeback(self, time: datetime.datetime) -> TextWithTTS:
         pass
 
+    @abc.abstractmethod
+    def get_menu_welcome_message(self) -> TextWithTTS:
+        pass
 
+    @abc.abstractmethod
+    def get_info_message(self) -> TextWithTTS:
+        pass
+    
+    @abc.abstractmethod
+    def get_ask_tip_topic_message(self) -> TextWithTTS:
+        pass
+
+    @abc.abstractmethod
+    def get_propose_yesterday_wake_up_time_message(self, last_time: datetime.time) -> TextWithTTS:
+        pass
+
+    @abc.abstractmethod
+    def get_ask_wake_up_time_message(self) -> TextWithTTS:
+        pass
+
+    @abc.abstractmethod
+    def get_ask_sleep_mode_message(self) -> TextWithTTS:
+        pass
+
+    @abc.abstractmethod
+    def get_tip_message(self, tip: Any) -> TextWithTTS:
+        pass
+
+    @abc.abstractmethod
+    def get_sleep_calc_time_message(self, bed_time: datetime.time,
+                                    activities: List[Any]) -> TextWithTTS:
+        pass
+
+    @abc.abstractmethod
+    def get_good_night_message(self) -> TextWithTTS:
+        pass
