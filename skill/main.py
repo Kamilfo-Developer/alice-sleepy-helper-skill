b8e196a4eafbcd62d95abfd974bca8ced40001fd
Kamil Biktimirov <Biktimirov.kamil@yandex.ru>
2023-03-22 06:16:33 +0300

    Added params from settings to run_app aiohttp method

--- skill/main.py
+++ skill/main.py
@@ -1,9 +1,9 @@
 from aiohttp import web
 from handlers import dp
-from config import WEBHOOK_URL_PATH
+from config import WEBHOOK_URL_PATH, WEBAPP_HOST, WEBAPP_PORT
 from aioalice import get_new_configured_app
 
 
 if __name__ == "__main__":
     app = get_new_configured_app(dispatcher=dp, path=WEBHOOK_URL_PATH)
-    web.run_app(app)
+    web.run_app(app, host=WEBAPP_HOST, port=int(WEBAPP_PORT))
