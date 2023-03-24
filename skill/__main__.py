21e1f2e5607ba86cff491ed07267f0e8251f9b28
Kamil Biktimirov <Biktimirov.kamil@yandex.ru>
2023-03-24 21:09:00 +0300

    Now the app can be launched with python -m skill command

---
+++ skill/__main__.py
@@ -0,0 +1,9 @@
+from aiohttp import web
+from skill.handlers import dp
+from skill.config import WEBHOOK_URL_PATH, WEBAPP_HOST, WEBAPP_PORT
+from aioalice import get_new_configured_app
+
+
+if __name__ == "__main__":
+    app = get_new_configured_app(dispatcher=dp, path=WEBHOOK_URL_PATH)
+    web.run_app(app, host=WEBAPP_HOST, port=int(WEBAPP_PORT))
