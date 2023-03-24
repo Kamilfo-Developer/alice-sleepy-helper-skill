9476a621ad9ede4b1cdba18d083b9b5c3ca47a08
Kamil Biktimirov <Biktimirov.kamil@yandex.ru>
2023-03-24 06:04:34 +0300

    Added test for repo

--- tests/db/sa_db_settings.py
+++ tests/db/sa_db_settings.py
@@ -1,6 +1,8 @@
+from pathlib import Path
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
-
+from sqlalchemy.engine import Engine
+from sqlalchemy import event
 from dotenv import load_dotenv
 
 import os
@@ -11,54 +13,60 @@ from skill.db.repos.sa_repo import SARepoConfig
 load_dotenv()
 
 
-# # Needed for some developing features
-# # Should be False when production
-# DEBUG = os.getenv("BOT_TOKEN") or False
+# Needed for some developing features
+# Should be False when production
+DEBUG = os.getenv("BOT_TOKEN") or False
 
-# DB_PROVIDER = os.getenv("DB_PROVIDER") or "sqlite"
+DB_PROVIDER = os.getenv("TEST_DB_PROVIDER") or "sqlite"
 
-# SQLITE_DRIVER_NAME = os.getenv("SQLITE_DRIVER_NAME") or "aiosqlite"
+match DB_PROVIDER:
+    case "postgres":
+        POSTGRES_DRIVER_NAME = (
+            os.getenv("TEST_POSTGRES_DRIVER_NAME") or "asyncpg"
+        )
 
-# SQLITE_DB_NAME = os.getenv("SQLITE_DB_NAME") or "data.db"
+        POSTGRES_DB_NAME = os.getenv("TEST_POSTGRES_DB_NAME") or "postgres"
 
-# ROOT_DIR = Path(__file__).parent.parent.parent.resolve()
+        POSTGRES_USER = os.getenv("TEST_POSTGRES_USER") or "postgres"
 
-# # In case you want to change path to SQLite DB file,
-# # just change this variable
-# SQLITE_DB_FILE_PATH = os.getenv("SQLITE_DB_FILE_PATH") or os.path.join(
-#     ROOT_DIR, f"{SQLITE_DB_NAME}"
-# )
+        POSTGRES_HOST = os.getenv("TEST_POSTGRES_HOST") or "localhost"
 
-# # URL for your database
-# DB_URL = f"sqlite+{SQLITE_DRIVER_NAME}:///" + SQLITE_DB_FILE_PATH
+        POSTGRES_DB_PORT = os.getenv("TEST_POSTGRES_DB_PORT") or "5432"
+
+        POSTGRES_PASSWORD = os.getenv("TEST_POSTGRES_PASSWORD") or "postgres"
+
+        if not POSTGRES_PASSWORD:
+            raise EnvironmentError(
+                "POSTGRES_PASSWORD required if you are using PostgreSQL"
+            )
+
+        DB_URL = (
+            f"postgresql+{POSTGRES_DRIVER_NAME}://"
+            + f"{POSTGRES_USER}:{POSTGRES_PASSWORD}"
+            + f"@{POSTGRES_HOST}:{POSTGRES_DB_PORT}"
+            + f"/{POSTGRES_DB_NAME}"
+        )
+
+    case _:
+        SQLITE_DRIVER_NAME = (
+            os.getenv("TEST_SQLITE_DRIVER_NAME") or "aiosqlite"
+        )
+
+        SQLITE_DB_NAME = os.getenv("TEST_SQLITE_DB_NAME") or "test_data.db"
+
+        ROOT_DIR = Path(__file__).parent.parent.parent.resolve()
+
+        # In case you want to change path to SQLite DB file,
+        # just change this variable
+        SQLITE_DB_FILE_PATH = os.getenv(
+            "TEST_SQLITE_DB_FILE_PATH"
+        ) or os.path.join(ROOT_DIR, f"{SQLITE_DB_NAME}")
+
+        # URL for your database
+        DB_URL = f"sqlite+{SQLITE_DRIVER_NAME}:///" + SQLITE_DB_FILE_PATH
 
 
-POSTGRES_DRIVER_NAME = os.getenv("TEST_POSTGRES_DRIVER_NAME") or "asyncpg"
-
-POSTGRES_DB_NAME = os.getenv("TEST_POSTGRES_DB_NAME") or "postgres"
-
-POSTGRES_USER = os.getenv("TEST_POSTGRES_USER") or "postgres"
-
-POSTGRES_HOST = os.getenv("TEST_POSTGRES_HOST") or "localhost"
-
-POSTGRES_DB_PORT = os.getenv("TEST_POSTGRES_DB_PORT") or "5432"
-
-POSTGRES_PASSWORD = os.getenv("TEST_POSTGRES_PASSWORD") or "postgres"
-
-if not POSTGRES_PASSWORD:
-    raise EnvironmentError(
-        "POSTGRES_PASSWORD required if you are using PostgreSQL"
-    )
-
-DB_URL = (
-    f"postgresql+{POSTGRES_DRIVER_NAME}://"
-    + f"{POSTGRES_USER}:{POSTGRES_PASSWORD}"
-    + f"@{POSTGRES_HOST}:{POSTGRES_DB_PORT}"
-    + f"/{POSTGRES_DB_NAME}"
-)
-
-
-engine = create_async_engine(DB_URL, echo=True)
+engine = create_async_engine(DB_URL, echo=False)
 
 
 async_session = sessionmaker(  # type: ignore
@@ -68,10 +76,10 @@ async_session = sessionmaker(  # type: ignore
 sa_repo_config = SARepoConfig(connection_provider=async_session)
 
 
-# if DB_PROVIDER == "sqlite":
+if DB_PROVIDER == "sqlite":
 
-#     @event.listens_for(Engine, "connect")
-#     def set_sqlite_pragma(dbapi_connection, connection_record):
-#         cursor = dbapi_connection.cursor()
-#         cursor.execute("PRAGMA foreign_keys=ON")
-#         cursor.close()
+    @event.listens_for(Engine, "connect")
+    def set_sqlite_pragma(dbapi_connection, connection_record):
+        cursor = dbapi_connection.cursor()
+        cursor.execute("PRAGMA foreign_keys=ON")
+        cursor.close()
