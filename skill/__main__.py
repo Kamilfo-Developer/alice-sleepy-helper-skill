f19d5f0605e4e1279f519a289e3fcb2c92337d95
AlexWolfov <git_user@mail.ru>
2023-03-25 13:44:58 +0300

    1) Handlers moved to ```handlers.py```. 2) Added ```test_api``` for testing handlers

---
+++ skill/__main__.py
@@ -0,0 +1,8 @@
+from aiohttp import web
+from skill.config import WEBHOOK_URL_PATH, WEBAPP_HOST, WEBAPP_PORT
+from aioalice import get_new_configured_app
+from skill.handlers import dp
+
+if __name__ == "__main__":
+    app = get_new_configured_app(dispatcher=dp, path=WEBHOOK_URL_PATH)
+    web.run_app(app, host=WEBAPP_HOST, port=int(WEBAPP_PORT), loop=dp.loop)
